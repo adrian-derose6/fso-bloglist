d462fc192f02551547ffc27ba3c9d3d3456996ed
Adrian DeRose <adrianderose.dev@gmail.com>
2024-12-22 17:15:33 -0800

    add middleware to app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 // Utils
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 // Routes
 const blogsRouter = require('./controllers/blogs')
@@ -24,8 +25,13 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
