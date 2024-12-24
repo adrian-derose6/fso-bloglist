52bc84e4988b91760917adac9b1e6a5021f41bed
Adrian DeRose <adrianderose.dev@gmail.com>
2024-12-23 17:10:08 -0800

    convert controllers from promise chains to async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,23 +1,43 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      console.log(blogs)
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  console.log(blogs)
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    }
+    else {
+      response.status(400).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
+})
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async (request, response, next) => {
+  const blog = new Blog(request.body)
+  try {
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- tests/blogApi.test.js
+++ tests/blogApi.test.js
@@ -5,51 +5,13 @@ const app = require('../app')
 const assert = require('assert')
 
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-  },
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-  },
-  {
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  },
-  {
-    title: "First class tests",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-    likes: 10,
-  },
-  {
-    title: "TDD harms architecture",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-    likes: 0,
-  },
-  {
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    likes: 2,
-  }  
-]
+const { nonExistingId, initialBlogs, blogsInDb } = require('./testHelper')
 
 const api = supertest(app)
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  const blogs = initialBlogs.map(async(blog, i) => {
+  const blogs = initialBlogs.map(async (blog) => {
     let blogObject = new Blog(blog)
     await blogObject.save()
   })
@@ -63,10 +25,10 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('theren are six blogs', async () => {
-  const response = await api.get('/api/blogs')
+test('all blogs are returned', async () => {
+  const allBlogs = await blogsInDb()
 
-  assert.strictEqual(response.body.length, initialBlogs.length)
+  assert.strictEqual(allBlogs.length, initialBlogs.length)
 })
 
 test('the first blog is about React patterns', async () => {
@@ -74,6 +36,70 @@ test('the first blog is about React patterns', async () => {
 
   const title = response.body.map(e => e.title)
   assert.strictEqual(title.includes('React patterns'), true)
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Introduction to JavaScript Development",
+    author: "Adrian DeRose",
+    url: "https://javascriptdev.com/",
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, initialBlogs.length + 1)
+  
+  const titles = blogsAtEnd.map(r => r.title)
+  assert(titles.includes('Introduction to JavaScript Development'))
+})
+
+test('blog without a title is not added', async () => {
+  const newBlog = {
+    author: "Adrian DeRose",
+    url: "https://javascriptdev.com/",
+    likes: 10,
+  }
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, initialBlogs.length)
+})
+
+test('a specific blog can be viewed', async () => {
+  const blogsAtStart = await blogsInDb()
+  const blogToView = blogsAtStart[0]
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+  
+  assert.deepStrictEqual(resultBlog.body, blogToView)
+})
+
+test('a note can be deleted', async () => {
+  const blogsAtStart = await blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await blogsInDb()
+
+  const titles = blogsAtEnd.map(r => r.title)
+  assert(!titles.includes(blogToDelete.title))
+
+  assert.strictEqual(blogsAtEnd.length, initialBlogs.length - 1)
 })
 
 
